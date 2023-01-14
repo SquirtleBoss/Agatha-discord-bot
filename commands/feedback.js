81e77002eebb03d1d4810b487c17b9f4ae093de5
xducky <36997649+xducky@users.noreply.github.com>
2023-01-14 00:01:10 -0800

    upgraded all commands to GPT3.5

--- commands/feedback.js
+++ commands/feedback.js
@@ -7,7 +7,7 @@ module.exports = {
 		.setDescription('Provides feedback on a piece of formal writing')
 		.addStringOption(option => option.setName('input').setDescription('Paste your writing here')),
 	async execute(interaction) {
-		var string = "Provide feedback on the following writing and how it can be improved:";
+		var string = "Provide feedback on the following writing and explain how it can be improved:";
 		linkai.callOpenAI(string, "text-davinci-003", interaction, response_len=512);
 	}
 };
