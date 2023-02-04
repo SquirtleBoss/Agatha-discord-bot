91875bdef0540c23d091f14b72d7d8f74fae8936
xducky <36997649+xducky@users.noreply.github.com>
2023-02-03 22:58:34 -0800

    /help fix hopefully last

--- commands/help.js
+++ commands/help.js
@@ -12,9 +12,6 @@ module.exports = {
         console.log('commandFiles');
         for (const file of commandFiles) {
             const command = require(`./${file}`);
-            // Set a new item in the Collection
-            // With the key as the command name and the value as the exported module
-            client.commands.set(command.data.name, command);
             ret += `Name: ${command.data.name}, Description: ${command.data.description} \n`;
         }
         console.log(ret);
