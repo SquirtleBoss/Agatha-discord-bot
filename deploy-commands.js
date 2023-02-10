aa376475f65bf19cec540fb41687507807925b77
xducky <36997649+xducky@users.noreply.github.com>
2023-02-09 23:43:49 -0800

    Switched from GuildCommands to GlobalCommands

--- deploy-commands.js
+++ deploy-commands.js
@@ -10,10 +10,11 @@ const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('
 for (const file of commandFiles) {
 	const command = require(`./commands/${file}`);
 	commands.push(command.data.toJSON());}
+	console.log(commands);
 
 
 const rest = new REST({ version: '9' }).setToken(process.env.token);
 
-rest.put(Routes.applicationGuildCommands(process.env.clientId, process.env.guildId), { body: commands })
+rest.put(Routes.applicationCommands(process.env.clientId), { body: commands })
 	.then(() => console.log('Successfully registered application commands.'))
 	.catch(console.error);
