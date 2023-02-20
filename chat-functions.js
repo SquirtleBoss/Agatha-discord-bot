61354d9b99936451ee90aa12a11d3ae0e0d02f02
xducky <36997649+xducky@users.noreply.github.com>
2023-02-20 04:13:07 -0800

    fixed return undefined bug

--- chat-functions.js
+++ chat-functions.js
@@ -1,18 +1,15 @@
 var Airtable = require('airtable');
 var base = new Airtable({apiKey: process.env.airtablekey}).base(process.env.airtablebase);
 
+var recordlist;
+
 async function retrieveRecord (user)  {
-    base('Chats').select({
+    const ret = await base('Chats').select({
         // Selecting the first 3 records in Grid view:
         maxRecords: 1,
         filterByFormula: "{ID} = '"+ user + "'"
-    }).eachPage(function page(records, fetchNextPage) {
-        console.log(records);
-        //This function (`page`) will get called for each page of records.
-        return records;
-    }, function done(err) {
-        if (err) { console.error(err); return; }
-    });
+    }).firstPage();
+    return ret;
 }
 
 module.exports = {retrieveRecord};
