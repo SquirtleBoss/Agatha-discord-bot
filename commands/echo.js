115f46302e3ae2fd183c3bc36cb8f0df307ef434
xducky <36997649+xducky@users.noreply.github.com>
2023-04-03 23:50:52 -0700

    fix echo command

--- commands/echo.js
+++ commands/echo.js
@@ -3,7 +3,8 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('echo')
-		.setDescription('Replies with your input'),
+		.setDescription('Replies with your input')
+        .addStringOption(option => option.setName('input').setDescription('Your message here')),
 	async execute(interaction) {
         const ret = interaction.options.getString('input');
 		await interaction.reply(ret);
